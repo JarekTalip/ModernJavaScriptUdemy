5a06819c04ac1adcee595fad64ac83910250698f
Jaroslaw Pilat <pilatjaroslaw@gmail.com>
2019-03-24 20:44:00 +0100

    Numbers Guesser final files

--- NumberGuessr_Project/app.js
+++ NumberGuessr_Project/app.js
@@ -2,7 +2,7 @@
 
 let min =1,
     max = 10,
-    winningNumber = 2,
+    winningNumber = getWinningNum(min, max),
     guessLeft =3;
 
 // Create UI elements
@@ -17,6 +17,13 @@ const   game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+//Play again event listener
+game.addEventListener('mousedown', (e) => {
+    if(e.target.className === 'play-again') {
+        window.location.reload();
+    }
+} );
+
 // Add listener for guess
 guessBtn.addEventListener('click', function() {
     let guess = parseInt(guessInput.value);
@@ -24,19 +31,19 @@ guessBtn.addEventListener('click', function() {
     // Validation of input.value
     if(isNaN(guess)|| guess < min || guess > max) {
         displayMessage(`Proszę wprowadź liczbę pomiędzy ${min} i ${max}`, 'red');
+        return false;
     }
 
     if(guess === winningNumber) {
-        guessInput.diabled = true;
-        guessInput.style.backgroundColor = "green";
-        displayMessage(`Liczba ${guess} to poprawna odpowiedź - BRAWOOOO!`, 'green')
+        
+        gameOver(true, `Liczba ${guess} to poprawna odpowiedź - BRAWOOOO!`)
+        
     } else {
         guessLeft -=1;
 
         if(guessLeft === 0) {
-            guessInput.diabled = true;
-            guessInput.style.backgroundColor = "red";
-            displayMessage(`Gra skończona. Wygrywająca liczba to ${winningNumber} - PRZEGRAŁEŚ`, "red");
+            
+            gameOver(false, `Gra skończona. Wygrywająca liczba to ${winningNumber} - SPRÓBUJ JESZCZE RAZ.`);
         } 
         else {
             guessInput.diabled = true;
@@ -61,4 +68,24 @@ function displayMessage (msg, color) {
     message.style.color = color;
     message.style.fontWeight = 700;
     message.textContent = msg;
+}
+
+// Game over function
+function gameOver (ifWon, msg) {
+    let color;
+
+    ifWon === true ? color = 'green' : color = 'red';
+
+    guessInput.diabled = true;
+    guessInput.style.backgroundColor = color;
+    displayMessage(msg, color);
+
+    //Play again
+    guessBtn.value = "Zagraj jeszcze raz!"
+    guessBtn.className += "play-again";
+}
+
+// getWinningNum function
+function getWinningNum(min, max) {
+    return Math.floor((Math.random() * (max-min +1))+min);
 }
